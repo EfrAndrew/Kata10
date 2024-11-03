80464ab2c5bdc714437a1189968be72fc49c26fb
EfrAndrew <43650345+EfrAndrew@users.noreply.github.com>
2024-11-03 03:08:45 +0300

    Add files via upload

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,40 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import web.model.User;
+
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void delete(Long id) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) entityManager.remove(user);
+    }
+
+    @Override
+    public void update(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("From User", User.class).getResultList();
+    }
+}
